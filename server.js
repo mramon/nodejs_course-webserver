6b3ad2457029cb78568aab1bcdc71c30d5b0b222
MIGUEL ANGEL RAMON LEON <maramon@santander.com.mx>
2020-11-10 10:41:27 -0600

    init version.

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+ 
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// app.get('/', (req, res) => {
+//     let salida = {
+//         nombre: 'miguel',
+//         edad: 100,
+//         url: req.url
+//     };
+//     res.send(salida)
+// })
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'miKe'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.listen(port, () => {console.log(`Escuchando peticiones en el puerto {$port}...`)})
+
